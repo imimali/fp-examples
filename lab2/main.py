0088384470d0350508786e05f5ba8dea8f17fc36
imimali <imre.mali@sectorlabs.ro>
2021-10-07 16:53:47 +0300

    Step 3: Add skeleton for a functionality

--- lab2/main.py
+++ lab2/main.py
@@ -15,10 +15,27 @@ def read_command():
     return int(input("Enter command: "))
 
 
+def find_odd(elements):
+    """
+    Finds all elments that are odd in a list of numbers
+    :param elements: list of integer numbers
+    :return: a list with all odd numbers from elements
+    """
+    return []
+
+
+def test_find_odd():
+    assert find_odd([]) == []
+
+
 def ui():
+    numbers = [1, 2, 5, 8, 10, 45, 9, 2]
     while True:
         print_menu()
         command = read_command()
+        if command == 1:
+            odd_numbers = find_odd(numbers)
+            print("Odd numbers are", odd_numbers)
         if command == 0:
             print("Bye bye!")
             break
